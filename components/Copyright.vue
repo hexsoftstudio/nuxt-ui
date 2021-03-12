54137ec404f8872bcb01909bf607c56e73e872ca
sirkitts <sirkitts@gmail.com>
2021-03-12 23:43:42 +1100

    make copyright dynamic

--- components/Copyright.vue
+++ components/Copyright.vue
@@ -1,14 +1,37 @@
 <template>
-    <footer>
-        <div class="footer-gold">Copyright (c) 2021</div>
-    </footer>
+  <div class="footer__copyright">
+    <span>© {{ copytext }} {{ text }} {{ abn }}</span>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Copyright'
+  name: 'Copyright',
+  props: {
+    company: {
+      type: String,
+      default: '',
+    },
+    abn: {
+      type: String,
+      default: '',
+    },
+    text: {
+      type: String,
+      default: 'All Rights Reserved.',
+    },
+  },
+  data() {
+    return {
+      year: 2021,
+      copytext: '',
+    }
+  },
+  mounted() {
+    const space = ' '
+    const d = new Date()
+    this.year = d.getFullYear()
+    this.copytext = this.year + (this.company ? space : '') + this.company + '.'
+  },
 }
 </script>
-
-<style>
-</style>
